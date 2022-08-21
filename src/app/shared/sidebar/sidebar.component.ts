0a93ffcbbeaed54d203e4001bbf877a4ac0092e6
Jose Carlos <josecarlos@uimunicipalistas.org>
2022-08-21 13:46:46 +0200

    Inicial

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  constructor(private _gifsService: GifsService){}
+  get historial():string[]{
+    return this._gifsService.historial;
+  }
+
+  buscar( termino:string){
+    console.log(termino);
+    this._gifsService.buscarGifs(termino);
+  }
+}
